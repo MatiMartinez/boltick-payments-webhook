f09ed1cb095e9d4e5c59ccf8dc71e198c7e3015c
Matias <martinezmatiemi@gmail.com>
2025-08-06 20:58:16 -0300

    .

--- src/app/useCases/UpdatePaymentUseCase/UpdatePaymentUseCase.ts
+++ src/app/useCases/UpdatePaymentUseCase/UpdatePaymentUseCase.ts
@@ -14,9 +14,7 @@ export class UpdatePaymentUseCase implements IUpdatePaymentUseCase {
   ) {}
 
   async execute(input: MercadopagoWebhookInput) {
-    const mercadoPagoPayment = await this.MercadoPagoService.getPayment(
-      input.data.id
-    );
+    const mercadoPagoPayment = await this.MercadoPagoService.getPayment(input.data.id);
 
     if (
       !mercadoPagoPayment ||
@@ -24,16 +22,11 @@ export class UpdatePaymentUseCase implements IUpdatePaymentUseCase {
       mercadoPagoPayment.status !== "approved" ||
       !mercadoPagoPayment.live_mode
     ) {
-      console.log(
-        "Mercado Pago payment not found: ",
-        JSON.stringify({ input, mercadoPagoPayment })
-      );
+      console.log("Mercado Pago payment not found: ", JSON.stringify({ input, mercadoPagoPayment }));
       return true;
     }
 
-    const payment = await this.PaymentRepository.getPaymentById(
-      mercadoPagoPayment.external_reference
-    );
+    const payment = await this.PaymentRepository.getPaymentById(mercadoPagoPayment.external_reference);
 
     if (!payment) {
       console.log("Payment not found: ", JSON.stringify({ input, payment }));
@@ -45,42 +38,44 @@ export class UpdatePaymentUseCase implements IUpdatePaymentUseCase {
       return true;
     }
 
-    const updatedPayment = await this.PaymentRepository.updatePayment(
-      payment.userId,
-      payment.createdAt,
-      {
-        paymentStatus: this.generatePaymentStatus(mercadoPagoPayment.status),
+    const updatedPayment = await this.PaymentRepository.updatePayment(payment.userId, payment.createdAt, {
+      paymentStatus: this.generatePaymentStatus(mercadoPagoPayment.status),
+      updatedAt: new Date().getTime(),
+      paymentDetails: {
+        amount: mercadoPagoPayment.transaction_amount as number,
+        code: mercadoPagoPayment.id?.toString() as string,
+        id: mercadoPagoPayment.id?.toString() as string,
         updatedAt: new Date().getTime(),
-        paymentDetails: {
-          amount: mercadoPagoPayment.transaction_amount as number,
-          code: mercadoPagoPayment.id?.toString() as string,
-          id: mercadoPagoPayment.id?.toString() as string,
-          updatedAt: new Date().getTime(),
-        },
-      }
-    );
+      },
+    });
 
     if (updatedPayment.paymentStatus !== "Approved") {
       return true;
     }
 
-    await this.TicketCountRepository.incrementCountByEventId(
-      updatedPayment.eventId,
-      updatedPayment.nfts.length
-    );
+    await this.TicketCountRepository.incrementCountByEventId(updatedPayment.eventId, updatedPayment.nfts.length);
 
-    await Promise.all(
-      updatedPayment.nfts.map(async (nft) => {
-        await this.EventBridgeService.sendEvent(
-          `SendNFTToWallet_${process.env.ENV}`,
-          "SEND_NFT",
-          {
-            id: updatedPayment.id,
-            nftId: nft.id,
-          }
-        );
-      })
-    );
+    // await Promise.all(
+    //   updatedPayment.nfts.map(async (nft) => {
+    //     await this.EventBridgeService.sendEvent(`SendNFTToWallet_${process.env.ENV}`, "SEND_NFT", {
+    //       id: updatedPayment.id,
+    //       nftId: nft.id,
+    //     });
+    //   })
+    // );
+
+    for (let i = 0; i < updatedPayment.nfts.length; i++) {
+      const nft = updatedPayment.nfts[i];
+
+      await this.EventBridgeService.sendEvent(`SendNFTToWallet_${process.env.ENV}`, "SEND_NFT", {
+        id: updatedPayment.id,
+        nftId: nft.id,
+      });
+
+      if (i < updatedPayment.nfts.length - 1) {
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+      }
+    }
 
     return true;
   }
